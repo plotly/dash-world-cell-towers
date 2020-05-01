cdefcb0c5ea8304f8ab605200a999120248d400b
Jon Mease <jon.mease@gmail.com>
2020-05-01 19:59:58 -0400

    remote prints

--- launch_dev.sh
+++ launch_dev.sh
@@ -2,7 +2,7 @@
 # Add this directory to PYTHONPATH
 export PYTHONPATH=$PYTHONPATH:`dirname "$(realpath $0)"`
 dask-scheduler &
-dask-worker 127.0.0.1:8786 --nprocs 1 --local-directory work-dir &
+dask-worker 127.0.0.1:8786 --nprocs 1 --nthreads 2 --local-directory work-dir &
 python publish_data.py
 python dash_opencellid/app.py
 wait
